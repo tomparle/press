b97483dae4e4ddb2648bff8feb429aada759dd55
Thomas Parle <thomas.parle@gmail.com>
2014-05-27 16:51:20 +0200

    fixed incorrect memcache delay value

--- app/press/io/InMemoryCompressedFile.java
+++ app/press/io/InMemoryCompressedFile.java
@@ -18,153 +18,162 @@ import press.PressException;
 import press.PressLogger;
 
 public class InMemoryCompressedFile extends CompressedFile {
-    private static final String FILE_LIST_KEY = "InMemoryFileList";
-    private InputStream inputStream;
-    private Writer writer;
-    private ByteArrayOutputStream outputStream;
-    private byte[] bytes;
-    private static final String A_VERY_LONG_TIME = "30d";
+	private static final String FILE_LIST_KEY = "InMemoryFileList";
+	private InputStream inputStream;
+	private Writer writer;
+	private ByteArrayOutputStream outputStream;
+	private byte[] bytes;
+	private static final String A_VERY_LONG_TIME = "30d";
 
-    public InMemoryCompressedFile(String fileKey) {
-        super(fileKey);
-    }
+	public InMemoryCompressedFile(String fileKey) {
+		super(fileKey);
+	}
 
-    @Override
-    public boolean exists() {
-        if (inputStream != null) {
-            return true;
-        }
+	@Override
+	public boolean exists() {
+		if (inputStream != null) {
+			return true;
+		}
 
-        long startTime = System.currentTimeMillis();
-        bytes = (byte[]) Cache.get(getCacheKey());
-        if (bytes != null) {
-            long totalTime = System.currentTimeMillis() - startTime;
-            PressLogger.trace("Got file of size %d bytes from cache in %d milli-seconds.",
-                    bytes.length, totalTime);
-            inputStream = new ByteArrayInputStream(bytes);
-            return true;
-        }
+		final long startTime = System.currentTimeMillis();
+		bytes = (byte[]) Cache.get(getCacheKey());
+		if (bytes != null) {
+			final long totalTime = System.currentTimeMillis() - startTime;
+			PressLogger.trace("Got file of size %d bytes from cache in %d milli-seconds.", bytes.length, totalTime);
+			inputStream = new ByteArrayInputStream(bytes);
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public InputStream inputStream() {
-        if (!exists()) {
-            throw new PressException("Can't create InputStream. File with key " + getCacheKey()
-                    + " does not exist in cache");
-        }
+	@Override
+	public InputStream inputStream() {
+		if (!exists()) {
+			throw new PressException("Can't create InputStream. File with key " + getCacheKey() + " does not exist in cache");
+		}
 
-        return inputStream;
-    }
+		return inputStream;
+	}
 
-    @Override
-    public String name() {
-        return FileIO.getFileNameFromPath(getFileKey());
-    }
+	@Override
+	public String name() {
+		return FileIO.getFileNameFromPath(getFileKey());
+	}
 
-    @Override
-    public Writer startWrite() {
-        // Compression might take a while, so if we're already writing out the
-        // compressed file from a different thread, return null
-        String inProgressKey = getInProgressKey(getFileKey());
-        if (Cache.get(inProgressKey) != null) {
-            return null;
-        }
-        String expiration = (PluginConfig.maxCompressionTimeMillis / 1000) + "s";
-        Cache.safeSet(inProgressKey, true, expiration);
+	@Override
+	public Writer startWrite() {
+		// Compression might take a while, so if we're already writing out the
+		// compressed file from a different thread, wait for it to finish and return null (=no writer)
+		final String inProgressKey = getInProgressKey(getFileKey());
 
-        if (writer == null) {
-            outputStream = new ByteArrayOutputStream();
-            try {
-                writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                throw new UnexpectedException(e);
-            }
-        }
+		// file already being generated
+		if (Cache.get(inProgressKey) != null) {
+			final long now = System.currentTimeMillis();
+			while (Cache.get(inProgressKey) != null) {
+				PressLogger.trace("Waiting for file to be generated: " + getFileKey());
+				if (System.currentTimeMillis() - now > PluginConfig.maxCompressionTimeMillis) {
+					throw new PressException("Timeout waiting for compressed file to be generated: " + inProgressKey);
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (final InterruptedException e) {
+				}
+			}
+			// return null to notify that no writer is used and the generated file is already ready to serve
+			return null;
+		}
+		final String expiration = (PluginConfig.maxCompressionTimeMillis / 1000) + "s";
+		Cache.safeSet(inProgressKey, true, expiration);
 
-        return writer;
-    }
+		if (writer == null) {
+			outputStream = new ByteArrayOutputStream();
+			try {
+				writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+			} catch (final UnsupportedEncodingException e) {
+				throw new UnexpectedException(e);
+			}
+		}
 
-    @Override
-    public void close() {
-        if (writer == null) {
-            throw new PressException(
-                    "Output stream has not yet been created. Call getWriter() and write to it.");
-        }
-        try {
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            throw new UnexpectedException(e);
-        }
+		return writer;
+	}
 
-        byte[] outBytes = outputStream.toByteArray();
-        PressLogger.trace("Saving file of size %d bytes to cache.", outBytes.length);
-        addFileToCache(getFileKey(), outBytes);
+	@Override
+	public void close() {
+		if (writer == null) {
+			throw new PressException("Output stream has not yet been created. Call getWriter() and write to it.");
+		}
+		try {
+			writer.flush();
+			writer.close();
+		} catch (final IOException e) {
+			throw new UnexpectedException(e);
+		}
 
-        String inProgressKey = getInProgressKey(getFileKey());
-        Cache.safeDelete(inProgressKey);
-    }
+		final byte[] outBytes = outputStream.toByteArray();
+		PressLogger.trace("Saving file of size %d bytes to cache.", outBytes.length);
+		addFileToCache(getFileKey(), outBytes);
 
-    private static String getInProgressKey(String fileKey) {
-        return "in-progress-" + fileKey;
-    }
+		final String inProgressKey = getInProgressKey(getFileKey());
+		Cache.safeDelete(inProgressKey);
+	}
 
-    private String getCacheKey() {
-        return getCacheKey(getFileKey());
-    }
+	private static String getInProgressKey(String fileKey) {
+		return "in-progress-" + fileKey;
+	}
 
-    private static String getCacheKey(String fileKey) {
-        return "file-" + fileKey;
-    }
+	private String getCacheKey() {
+		return getCacheKey(getFileKey());
+	}
 
-    private void addFileToCache(String fileKey, byte[] outBytes) {
-        long startTime = System.currentTimeMillis();
+	private static String getCacheKey(String fileKey) {
+		return "file-" + fileKey;
+	}
 
-        Set<String> fileList = getFileList();
-        fileList.add(fileKey);
-        Cache.set(FILE_LIST_KEY, fileList, A_VERY_LONG_TIME);
+	private void addFileToCache(String fileKey, byte[] outBytes) {
+		final long startTime = System.currentTimeMillis();
 
-        String cacheKey = getCacheKey(fileKey);
-        if (!Cache.safeSet(cacheKey, outBytes, A_VERY_LONG_TIME)) {
-            throw new PressException(
-                    "Underlying cache implementation could not store compressed file " + fileKey
-                            + " in cache");
-        }
+		final Set<String> fileList = getFileList();
+		fileList.add(fileKey);
+		Cache.set(FILE_LIST_KEY, fileList, A_VERY_LONG_TIME);
 
-        inputStream = null;
-        bytes = null;
+		final String cacheKey = getCacheKey(fileKey);
+		if (!Cache.safeSet(cacheKey, outBytes, A_VERY_LONG_TIME)) {
+			throw new PressException("Underlying cache implementation could not store compressed file " + fileKey + " in cache");
+		}
 
-        long totalTime = System.currentTimeMillis() - startTime;
-        PressLogger.trace("Saved file to cache in %d milli-seconds", totalTime);
-    }
+		inputStream = null;
+		bytes = null;
 
-    public static int clearMemoryCache(String extension) {
-        Set<String> files = getFileList();
-        for (String fileKey : files) {
-            Cache.delete(getCacheKey(fileKey));
-            Cache.delete(getInProgressKey(fileKey));
-        }
-        Cache.delete(FILE_LIST_KEY);
-        return files.size();
-    }
+		final long totalTime = System.currentTimeMillis() - startTime;
+		PressLogger.trace("Saved file to cache in %d milli-seconds", totalTime);
+	}
 
-    private static Set<String> getFileList() {
-        Set<String> fileList = (Set<String>) Cache.get(FILE_LIST_KEY);
-        if (fileList == null) {
-            fileList = new HashSet<String>();
-        }
-        return fileList;
-    }
+	public static int clearMemoryCache(String extension) {
+		final Set<String> files = getFileList();
+		for (final String fileKey : files) {
+			Cache.delete(getCacheKey(fileKey));
+			Cache.delete(getInProgressKey(fileKey));
+		}
+		Cache.delete(FILE_LIST_KEY);
+		return files.size();
+	}
 
-    @Override
-    public long length() {
-        if (!exists()) {
-            throw new PressException("Can't get length. File with key " + getCacheKey()
-                    + " does not exist in cache");
-        }
+	private static Set<String> getFileList() {
+		Set<String> fileList = (Set<String>) Cache.get(FILE_LIST_KEY);
+		if (fileList == null) {
+			fileList = new HashSet<String>();
+		}
+		return fileList;
+	}
 
-        return bytes.length;
-    }
+	@Override
+	public long length() {
+		if (!exists()) {
+			throw new PressException("Can't get length. File with key " + getCacheKey() + " does not exist in cache");
+		}
+
+		return bytes.length;
+	}
 }
+
